9d1689b62bcfe71ecb5a6f7cf51557718d663c1d
halaabushehab <hala.abushiihab@gmail.com>
2024-08-21 20:46:10 +0300

    ....

--- main.js
+++ main.js
@@ -272,44 +272,15 @@ If you need a paragraph on a specific topic, feel free to ask!`,
 mainRecipes.forEach((ele, i) => {
   console.log(ele);
 
-  const thelist = $(`<div class="category">${ele.category}</div>`);
+  const thelist = $(`<div id="list_need${[i]} "  class="category">${ele.category}</div>`);
   mainType.append(thelist);
 });
 
 allrecipes.forEach((ele, i) => {
-  console.log(ele);
-
   const all_list =
-    $(`<div class="all"> <div class="border"> <img  class="img_size" src= "${ele.img}"/> <h3 class="names"> ${ele.name} </h3> 
+    $(`<div id="dish ${[i]}"   class="all"> <div class="border"> <img  class="img_size" src= "${ele.img}"/> <h3 class="names"> ${ele.name} </h3> 
         </div>
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        <div class="container"> 
+<div class="container"> 
         <h6>rateing</h6>
 <div class="star">
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
@@ -328,11 +299,147 @@ allrecipes.forEach((ele, i) => {
   <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
 </svg>
 
-        </div> </div> 
+        </div> 
         
+       <button id ="addtofav${[i]}  ">favourite </button>
+        
+        
+        
+        </div>  
  `);
   meals.append(all_list);
 });
+
+
+
+ const show_contant =()=>{
+   $('#dish1').on("click" ,()=>{
+console.log("hi")
+    $('.meals').hide();
+
+    // mainRecipes.forEach((ele, i) => {
+    //     const dish = $(`<div>${ele.name} ${ele.Ingredients}</div>`);
+    //   $(all1).append(dish);
+      
+    //   });
+     show_contant()
+} )
+
+}
+
+ show_contant();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // $('.category') .on("click" ,()=>{
+    
+    //     mainRecipes.forEach((ele, i) => {
+    //         const thelist = $(`<div class="category">${ele. discription }</div>`);
+    //         mainType.append(thelist);
+    //       });
+    
+
+    // })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (level === maxlevel) {
+//     div.style.display = "none";
+//     end.style.display = "block";
+
+//     end.addEventListener("click", lettrs);}
+  
+
+// });
+// }
+
+// };
+
+// start.addEventListener("click", () => {
+// alert("welcome " );
+// info.style.display = "none";
+// div.style.display = "block";
+// });
+
+
+
+
+
+
+
+
+
+
 
 //  const stars=$('.star svg')
 
@@ -346,3 +453,9 @@ allrecipes.forEach((ele, i) => {
 // )
 
 // })
+
+
+
+
+
+
