ba630759faeb8c2434b1c97a209027050c60c4d5
halaabushehab <hala.abushiihab@gmail.com>
2024-08-22 11:51:30 +0300

    fav list +rate star

--- main.js
+++ main.js
@@ -14,7 +14,8 @@ var img_food = $("<img />", {
 img_food.appendTo($("#logo_food"));
 mainType.append(img_food);
 
-const search = $(`<input class="search" placeholder="ابحث عن وصفة"></input>`);
+const search = $(`<div class="search_list"> <label for="search">البحث</label>
+    <input  class="search" type="search" placeholder="ابحث عن وصفة"></input> <button id="looking"  >enter</button> </div>`);
 
 mainType.append(search);
 
@@ -26,12 +27,11 @@ const meals = $(`<div></div>`);
 meals.addClass("meals");
 main.append(meals);
 
-const help_link = $(`<div></div>`);
-chooseYor_list.addClass("help_link");
-main.append(help_link);
+
 
 const tag_tape = $(`<div></div>`);
 tag_tape.addClass("tag_tape");
+tag_tape.text( "https://info.wafa.ps/ar_page.aspx?id=2459  ")
 main.append(tag_tape);
 
 const mainRecipes = [
@@ -272,37 +272,34 @@ If you need a paragraph on a specific topic, feel free to ask!`,
 mainRecipes.forEach((ele, i) => {
   console.log(ele);
 
-  const thelist = $(`<div  "  class="category">${ele.category}</div>`);
+  const thelist = $(`<div  id="category${i}" class="category data-category ="category${i}  ">${ele.category}</div>`);
   mainType.append(thelist);
 });
 
 allrecipes.forEach((ele, i) => {
   const all_list =
-    $(`<div    class="all"> <div class="border"> <img  class="img_size" src= "${ele.img}"/> <h3 class="names"> ${ele.name} </h3> 
+    $(`<div  id="all${i}"class="all" data-list="list${i}"> <div class="border"> <img  class="img_size" src= "${ele.img}"/> 
+  
+        <h3 class="names"> ${ele.name} </h3> 
+  
+      
+        <button  class="fav" id ="addtofav${i}" data-fav="fav${i} ">favourite </button>
+        
         </div>
+
+
+
 <div class="container"> 
         <h6>rateing</h6>
 <div class="star">
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
-  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
-</svg>
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
-  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
-</svg>
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
-  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
-</svg>
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
-  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
-</svg>
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
-  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
-</svg>
-
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star"></span>
+<span class="fa fa-star"></span>
         </div> 
         
-       <button id ="addtofav${[i]}  ">favourite </button>
-        
+     
         
         
         </div>  
@@ -311,23 +308,20 @@ allrecipes.forEach((ele, i) => {
 });
 
 
-
- const show_contant =()=>{
-   $('ce').on("click" ,()=>{
+$(function(){
+    const your_fav_list= [];
+$('.fav').on('click' ,function(){
+const favId=$(this).attr('data-fav');
+console.log(this)
+your_fav_list.push("hello")
+console.log(your_fav_list)
 console.log("hi")
-    $('.meals').hide();
 
-    mainRecipes.forEach((ele, i) => {
-        const dish = $(`<div>${ele.name} ${ele.Ingredients}</div>`);
-      $(all1).append(dish);
-      
-      });
-     show_contant()
-} )
 
-}
 
- show_contant();
+
+
+})
 
 
 
@@ -341,57 +335,14 @@ console.log("hi")
 
 
 
-    // $('.category') .on("click" ,()=>{
-    
-    //     mainRecipes.forEach((ele, i) => {
-    //         const thelist = $(`<div class="category">${ele. discription }</div>`);
-    //         mainType.append(thelist);
-    //       });
-    
-
-    // })
 
 
 
 
 
-// if (level === maxlevel) {
-//     div.style.display = "none";
-//     end.style.display = "block";
-
-//     end.addEventListener("click", lettrs);}
-  
-
-// });
-// }
-
-// };
-
-// start.addEventListener("click", () => {
-// alert("welcome " );
-// info.style.display = "none";
-// div.style.display = "block";
-// });
 
 
 
-
-
-//  const stars=$('.star svg')
-
-// starse.forEach((elem,i)=>{
-// elem.on("click" ,()=>
-// stars.forEach((star ,i_1)=>{
-
-//     i >=i_1 ? star.addClass.add("active"):star.addClass.remove("active")
-
-// })
-// )
-
-// })
-
-
-
-
+})
 
 
