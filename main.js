3ba85b37d8ddf4cc6f1faacdbd1c139dd9685282
halaabushehab <hala.abushiihab@gmail.com>
2024-08-24 20:04:18 +0300

    new

--- main.js
+++ main.js
@@ -2,11 +2,9 @@ const main = $(`<div></div>`);
 main.addClass("main");
 const body = $("body");
 body.append(main);
-
 const mainType = $(`<div></div>`);
 mainType.addClass("main_Type");
 main.append(mainType);
-
 // var img_food = $("<img  />", {
 //   id: "logo_food",
 //   src: "f2b032a5464f8b065c4d7a3148f6e040.jpg",
@@ -15,25 +13,32 @@ main.append(mainType);
 // mainType.append(img_food);
 
 const search = $(`
-    <label for="search">Search </label>
+    <label for="search">البحث</label>
 <input type="search" id="search" name="search" placeholder="ابحث عن الوصفه"/><br>
     
     `);
 
 mainType.append(search);
 
+
+
 const chooseYor_list = $(
-  `<div> 
-  <i  id="fav_list" class="fa fa-heart" style="font-size:20px;color:red"  ></i></div>`
-);
-search.append(chooseYor_list);
+    `<div> 
+    <i  id="fav_list" class="fa fa-heart" style="font-size:20px;color:red"  ></i></div>`
+  );
+  search.append(chooseYor_list);
+
+
+
+
+
 
 const meals = $(`<div></div>`);
 meals.addClass("meals");
 main.append(meals);
-
 const tag_tape = $(`<div></div>`);
 tag_tape.addClass("tag_tape");
+
 
 const mainRecipes = [
   {
@@ -44,7 +49,6 @@ const mainRecipes = [
         img: "مقلوبة.webp",
         Ingredients: `<h2 >المكونات</h2>
 <br>
-
         <br> كوبان ونصف من الأرز قصير الحبة.<br> ثمرة كبيرة من الباذنجان.<br>. كيلو واحد من الدجاج المسلوق.<br> ثلاثة أكواب ونصف من مرق الدجاج.<br> ملعقتان صغيرتان من الملح.<br> ملعقة صغيرة من البهارات المشكلة. <br>نصف ملعقة صغيرة من مسحوق الكركم.<br> نصف ملعقة صغيرة من مطحون القرفة.<br> ربع ملعقة صغيرة من حب الهال المطحون.<br> ربع ملعقة صغيرة من الفلفل الأسود.<br> ملعقتان كبيرتان من زيت الذرة. <br>كمية مناسبة من زيت الذرة للقلي.<br> كمية قليلة من الصنوبر المقلي.
 `,
         discription: ` <h2>طريقة التحضير </h2><br>
@@ -56,7 +60,6 @@ const mainRecipes = [
         img: "مسخن.jpg",
         Ingredients: ` المكونات <br>  <br> دجاجتان مقطعتان إلى أنصاف.<br> بصلة متوسطة مقطعة إلى أرباع  . عودان من القرفة.<br> عشر حبات من الهيل الصحيح. ثلاث قطع من ورق الغار. <br> أربعة كيلو غرامات من البصل المفروم خشناً  .<br> ثلاثة أكواب من زيت الزيتون.<br> ملعقة كبيرة من الملح. ثلاث ملاعق كبيرة من السماق. <br>ملعقة صغيرة من الفلفل الأسود المطحون.<br> ثمانية أرغفة من الخبز الإيراني.<br> لبن زبادي <br>
 `,
-
         discription: `  <br>  طريقة التحضير <br><br>وضع الدجاج في قدر وغمره بالماء ورفعه على نار متوسطة حتى يغلي، <br>والتخلص من الرغوة. إضافة ورق الغار والقرفة والفلفل والهيل والبصل والملح والفلفل، <br>وترك الدجاج مدة نصف ساعة حتى ينضج. وضع البصل في قدر متوسط ورفعه على نار متوسطة<br> وتحريك البصل مدة دقيقتين، وإضافة إليه الزيت وتغطية القدر، وترك البصل حتى ينضج لمدة ثلث ساعة،<br> ثم التتبيل بالفلفل والملح والسماق. وضع قطع الدجاج في صينية فرن ودهنا بزيت قلي البصل، ثم إدخال صينية الدجاج أسفل شواية الفرن وتحميرها حتى تتحمر قطع الدجاج.<br> تغليف صينية فرن بورق الألمنيوم ثم وضع رغيف خبز في الصينية<br> وثنيه ثم غمس أطرافه بزيت قلي البصل، وفرد الرغيف ثم توزيع البصل المقلي عليه، ورش السماق والصنوبر عليه،<br> ثم تحمير الصينية بشواية الفرن لمدّة تتراوح من ثلاث إلى أربع دقائق فقط.<br> وضع الدجاج والأرغفة في طبق التقديم، وتقديم اللبن الزبادي مع المسخن.`,
       },
       {
@@ -70,7 +73,6 @@ const mainRecipes = [
 أربع أكواب من اللّبن ( الرّائب، الجّميد، مخيض اللّبن ) .<br>
  أربع أكواب من الماء السّاخن. ربع كوب من زيت الذّرة.<br>
 `,
-
         discription: `  طريقة التحضير <br>
  بدايةً قومي بتقطيع العكّوب، وفي قدرٍ على النّار ضعي ثلاث ملاعق من زيت الذّرة إضافةً إلى البصل، <br>
 شوّحيه قليلاً حتّى يذبل، ثمّ أضيفي قطع اللّحم وقلّبيها قليلاً حتّى يتغيّر لونها. أضيفي الماء السّاخن على <br>
@@ -87,7 +89,6 @@ const mainRecipes = [
       },
     ],
   },
-
   {
     category: "المقبلات ",
     recipes: [
@@ -95,7 +96,6 @@ const mainRecipes = [
         name: " فتة الحمص",
         img: "فتة حمص.jpg",
         Ingredients: `   المكونات <br>أوقية من الحمص حب. ثلاث ملاعق صغيرة من عصير الليمون.<br> فص كبير من الثوم المطحون ناعماً. <br>ربع ملعقة كبيرة من الكمون. ست ملاعق صغيرة من الطحينة.<br> ست ملاعق صغيرة من زيت الزيتون.<br> ست ملاعق صغيرة من الماء.<br> ربع ملعقة صغيرة من الملح. <br>رغيف متوسّط من الخبز العربي.<br> ربع كأس من البقدونس، المفروم ناعماً. أربع ملاعق صغيرة من اللوز.`,
-
         discription: `طريقة التحضير <br>نضع في وعاء عميق، عصير الليمون، والطحينة، والماء، والكمون، والثوم، والملح، <br>ونخلطهم جيداً بواسطة الخلاط الكهربائي. نضيف إلى الخليط السابق الحمص الحب، ولكن نترك منه أربع ملاعق صغيرة جانباً.<br> <br>نضيف زيت الزيتون إلى الخليط السابق، ونخلط مجدداً، حتى نحصل على خليط متجانس. نضع زيت الذرة في مقلاة صغيرة وعلى نار متوسطة، حتى يسخن الزيت قليلاً، ثم نضيف إليه اللوز، ونقلبه حتى يأخذ اللون الذهبي.<br> نضيف إلى خليط الحمص، اللوز المقليّ، والبقدونس، ولبن الزبادي، ونخلطهم جيداً بواسطة ملعقة خشبية كبيرة.<br> نضع فتة الحمص الجاهزة في طبق زجاجي، ونزيّن وجهها بالخبز المقلي، والكمية المتبقيّة من الحمص الحب، واللوز المقلي،<br> ثم نقدم الطبق ساخناً.
 `,
       },
@@ -119,14 +119,11 @@ const mainRecipes = [
         name: " المخللات",
         img: "مخللات.jpg",
         Ingredients: `  المكوّنات <br>اثنان كيلو غرام من الخيار ذي الحبّة الرفيعة.<br> سنون من الثوم (بالإمكان الاستغناء عنه).<br> محلول التخليل: ملعقة صغيرة ممتلئة ملح لكل كوب ماء.<br> نصف كوب خل.<br> ملعقة صغيرة سكر.<br> نصف ملعقة صغيرة ثوم مهروس.<br> نصف ملعقة صغيرة كزبرة مطحونة. <br>بضع حبّات من الفلفل الحار.<br> نصف ملعقة صغيرة فليفلة حمراء مطحونة.<br>`,
-
         discription: ` طريقة التحضير<br>  نعبئ الخيار في وعاء مناسب مع حبات الثوم.<br> تلحضير محلول التخليل: نملأ وعاء الخيار بالماء تدريجياً (كوباً كوباً) وذلك لعدّها لغايات إضافة الملح، نستمر بالإضافة حتى يغمر الماء حبات الخيار مع الانتباه إلى عدم ملء<br> الإناء بشكل كامل وترك مساحة للخل. نعيد تفريغ الماء في وعاء آخر عميق، ثمّ نضيف الملح بحسب ما هو مذكور في المكوّنات: (ملعقة صغيرة ممتلئة من الملح لكل كوب ماء ).<br> نضيف السكر والتوابل:<br> (ثوم، وكزبرة، وفليفلة، وفلفل حار ) والخل، ونحرّك جيداً حتّى يذوب الملح. نصبّ محلول التخليل في وعاء الخيار، ثمّ نغلق الإناء (المرطبان) جيداً ونتركه لعدّة أيام<br> حتّى تتم عملية التخليل. نحفظه في الثلاجة عند انتهاء عملية التخليل حتّى لا يفسد
-
    `,
       },
     ],
   },
-
   {
     category: "  الحلويات",
     recipes: [
@@ -158,7 +155,6 @@ const mainRecipes = [
 ملعقة بيكينغ باودر   
   <br>
 3 أرباع كوب زيت
-
 كوب<br> ونصف ماء دافئ
 كوب ونصف قطر<br>
 <br>
@@ -177,7 +173,6 @@ const mainRecipes = [
       },
     ],
   },
-
   {
     category: " العصائر",
     recipes: [
@@ -191,27 +186,26 @@ const mainRecipes = [
         name: " الكركديه",
         img: "كركديه.jpg",
         Ingredients: `  المكونات <br>  كوبان من أوراق الكركديه المنقوعة في الماء المغلي.<br> علبة صغيرة من شرائح الأناناس. كوب من السكر،<br> أو حسب الرغبة.<br> ماء بارد.`,
-
         discription: `  طريقة التحضير <br>نصفّي أولاً عصير الكركديه ونتخلّص من الأوراق، ثمّ نضعه في وعاء جانباً.<br> نضع شرائح الأناناس في محضّرة الطعام مع عصيرها، ثمّ نخلط على سرعة متوسّطة.<br> نضيف عصير الكركديه والماء ونتابع الخلط، ثمّ نضيف السكر. نسكب عصير الكركديه والأناناس في أكواب التقديم، ونقدّمه بارداً.`,
       },
       {
         name: "العرقسوس ",
         img: " 879.webp  ",
         Ingredients: `لالمكوّنات  <br>ستة إلى ثمانية أكواب من الماء البارد.<br> كوب من عرق السوس المجفف. <br>ملعقة كبيرة من كربونات الصودا.`,
-
         discription: ` طريقة التحضير<br> وضع عرق السوس في وعاء مصنوع من الفخّار. إضافة ثلاث ملاعق كبيرة من الماء.<br> إضافة كربونات الصودا، ثمّ تقليب المزيج إلى أن تمتزج المكوّنات بشكل جيد، <br>ويصبح عرق السوس مبللاً وملتصقاً ببعضه البعض. تغطِية الوعاء باستخدام شاش نظيفة، ثمّ وضع الوعاء في مكان مشمس لمدّة تتراوح بين الساعة إلى ساعتين إلى أن يتخمر. وضع المزيج في قطع الشاش، <br>وربطها جيداً. <br>جلب طبقٍ عميقٍ، ووضع الماء البارد فيه، ثمّ وضع لفة عرق السوس به ونقعها لمدّة ساعة. إخراج اللفة من الوعاء الزجاجي.`,
       },
     ],
   },
 ];
 
+
+
+
 mainRecipes.forEach((e, i) => {
   const thelist = $(
     `<div  id="category${i}" class="category" data-category ="category${i}">${e.category}</div>`
   );
   mainType.append(thelist);
-
-
 
   const filter_det = $(`<div><ul> ${e.recipes.img}</ul></div>`);
   thelist.on("click", function () {
@@ -220,6 +214,7 @@ mainRecipes.forEach((e, i) => {
     console.log(e);
     $(`.main`).append(filter_det);
   });
+
 });
 
 
@@ -240,30 +235,26 @@ mainRecipes.forEach((e, i) => {
                 <img  class="img_size" src= "${e.img}"/> 
             <h3 class="names"> ${e.name} </h3> 
       </div>`);
-
     const content =
       $(`<button  class="fav" id ="addtofav${i}" data-fav="fav${i} ">
                 favourite 
         </button>`);
-
     const rating = $(`
         <div class="rating_box"> 
        <h4>Please rate the recipe</h4>
        <div class="stars">
-      <i data-star="star1"   id="star1" class="fa fa-star"  ></i>
-    <i data-star="star2"   id="star2" class="fa fa-star"></i>
-    <i data-star="star3"    id="star3" class="fa fa-star"></i>
-    <i data-star="star4"   id="star4" class="fa fa-star"></i>
-    <i data-star="star5"   id="star5"  class="fa fa-star"></i>
+      <i class="fa fa-star"></i>
+    <i class="fa fa-star"></i>
+    <i class="fa fa-star"></i>
+    <i class="fa fa-star"></i>
+    <i class="fa fa-star"></i>
          </div> 
          </div>  `);
 
+
     const container = $(`
-    <div id="show"  > </div> `);
-
-
-
-
+    <div id="show"  > </div>
+    `);
 
     //التقيم
     $(function () {
@@ -275,14 +266,14 @@ mainRecipes.forEach((e, i) => {
 // const starId=$(e).attr('data-star')
 // alert(starId )
 // rating.addClass.add("active")
- 
+
       });
       //             //     index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
     });
 
 
 
- 
+
 
 
 
@@ -293,8 +284,8 @@ mainRecipes.forEach((e, i) => {
     const detailes = $(`<div class="detailes">
     <h1  class="detailes_h"  >${e.name}</h1>
     <img  class="detailes_img" src= "${e.img}"/> 
-<p   class="detailes_p1" >${e.Ingredients}</p>
-<p   class="detailes_p2"   >${e.discription}</p>
+<p class="detailes_p1" >${e.Ingredients}</p>
+<p class="detailes_p2"   >${e.discription}</p>
     </div>`);
     imageDiv.on("click", function () {
       $(".meals").hide();
@@ -310,8 +301,10 @@ mainRecipes.forEach((e, i) => {
 
 
 
+
     //هون عم بشغل قائمة المفضله
     const working = () => {
+    
       const yourlist = $(`
     <button class="delet">deledt</button>
         `);
@@ -319,22 +312,52 @@ mainRecipes.forEach((e, i) => {
       content.on("click", function () {
         myfav.push(e);
         console.log(myfav);
+
         //هون عملت كليك كل ما اكبس على االقلب بطلع قائمة الاشياء المفضلة الي اخترتها
         $("#fav_list").on("click", () => {
-    
+
           meals.hide(500);
+          $(`.main`).append(imageDiv);
+
+          $(`.delet`).on("click", () => {
+            console.log("K")
+            myfav.forEach((toDelete, i) => {
+              if (e === toDelete) {
+                myfav.splice(i, 1);
+              }
+            });
           $(`.main`).append(imageDiv,content );
           });
           //هون بدي اعمل لما اكبس مرتين وهي بداخل صفحة fav list 
          // انو يطلعها من هناك 
-        //   $("#fav_list").on(" dblclick", () => {
-        //     myfav.splice(i, 1);
-        //     imageDiv.hide()
-        // });
+          $("#fav_list").on(" dblclick", () => {
+            myfav.splice(i, 1);
+            imageDiv.hide()
+        });
 
 
         });
-    
+
+function filterItems() {
+    const inp=[];
+    let input = $('#searchInput').val();
+    mainRecipes.filter((m, i) => {
+console.log(m)
+    console.log( "myitem" ,m)
+        if (m.textContent.includes(input)) {
+          inp.push(input)
+          console.log(inp)
+        } else {
+           m.hide;
+        }
+    })
+}
+filterItems();
+
+
+
+      });
+
 
       container.append(imageDiv);
       container.append(content);
@@ -342,6 +365,8 @@ mainRecipes.forEach((e, i) => {
       meals.append(container);
     };
     working();
+
+
 
     container.append(imageDiv);
     container.append(content);
@@ -352,6 +377,26 @@ mainRecipes.forEach((e, i) => {
 
 
 
+
+// console.log($(".stars"));
+
+// const ss=$(`<p></p>`)
+
+// $(`.stars span`).on("click", function(){
+
+//         console.log(i)
+
+//    })
+
+// $( '.rating').forEach((star, index1) => {
+//      star.on("click", () => {
+//  console.log("h")
+//      })
+//  });
+
+// $(function(){
+
+// })
 // $(document). ready(function(){
 //     $('stars'). click(function(){
 //     $('li i'). removeClass("active");
